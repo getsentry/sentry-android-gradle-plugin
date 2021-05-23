70544068419c44d987b518af4e7692eb581ea55b
Nicola Corti <corti.nico@gmail.com>
2021-05-23 16:10:02 +0200

    Add support for hashing the CLI (#110)
    Co-authored-by: Manoel Aranda Neto <5731772+marandaneto@users.noreply.github.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,7 @@
 
 * Silence the warning for missing mapping file on variants that don't enable minification (#111)
 * Bump: sentry-cli to 1.64.1
+* Add support for hashing sentry-cli to trigger a re-download upon bumps (#110)
 
 # 2.0.0-alpha.3
 
--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -5,12 +5,38 @@ repositories {
     jcenter()
 }
 
-val cliDir: Array<File> = File("./plugin-build/src/main/resources/bin/").listFiles() ?: emptyArray()
-if(cliDir.size == 1 && cliDir[0].name == ".gitignore") {
-    logger.lifecycle("Sentry CLI is Missing - Downloading it...")
+if(shouldDownloadSentryCli()) {
+    logger.lifecycle("Downloading Sentry CLI...")
     exec {
         executable("sh")
         workingDir("../plugin-build")
         args("-c", "./download-sentry-cli.sh")
+    }
+}
+
+/**
+ * When bumping the Sentry CLI, you should update the `expected-checksums.sha` file inside `buildSrc`
+ * to match the `checksums.sha` file from `./plugin-build/src/main/resources/bin/`
+ *
+ * That's to retrigger a download of the cli upon a bump.
+ */
+fun shouldDownloadSentryCli() : Boolean {
+    val cliDir: Array<File> = File("./plugin-build/src/main/resources/bin/").listFiles() ?: emptyArray()
+    val expectedChecksums = File("./buildSrc/expected-checksums.sha")
+    val actualChecksums = File("./plugin-build/src/main/resources/bin/checksums.sha")
+    return when {
+        cliDir.size <= 2 -> {
+            logger.lifecycle("Sentry CLI is missing")
+            true
+        }
+        !actualChecksums.exists() -> {
+            logger.lifecycle("Sentry CLI Checksums is missing")
+            true
+        }
+        expectedChecksums.readText() != actualChecksums.readText() -> {
+            logger.lifecycle("Sentry CLI Checksums doesn't match")
+            true
+        }
+        else -> false
     }
 }
