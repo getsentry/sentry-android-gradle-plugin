fb8e8564570a301f0a83cb78624c53f4ebeb94ef
iker barriocanal <32816711+iker-barriocanal@users.noreply.github.com>
2021-07-07 12:54:19 +0200

    Increment version and make it snapshot

--- scripts/post-release.sh
+++ scripts/post-release.sh
@@ -7,5 +7,24 @@ cd $SCRIPT_DIR/..
 OLD_VERSION="${1}"
 NEW_VERSION="${2}"
 
+GRADLE_FILEPATH="plugin-build/gradle.properties"
+
 # Add a new unreleased entry in the changelog
 sed -i "" 's/# Changelog/# Changelog\n\n## Unreleased/' CHANGELOG.md
+
+# Increment `version` and make it a snapshot
+# Incrementing the version before the release (`bump-version.sh`) sets a
+# fixed version until the next release it's made. For testing purposes, it's
+# interesting to have a different version name that doesn't match the
+# name of the version in production.
+# Note that the version must end with a number: `1.2.3-alpha` is a semantic
+# version but not compatible with this post-release script, and `1.2.3-alpha.0`
+# should be used instead.
+VERSION_NAME_PATTERN="version"
+version="$( awk "/$VERSION_NAME_PATTERN/" $GRADLE_FILEPATH | egrep -o '[0-9].*$' )" # from the first digit until the end
+version_digit_to_bump="$( awk "/$VERSION_NAME_PATTERN/" $GRADLE_FILEPATH | egrep -o '[0-9]+$')"
+((version_digit_to_bump++))
+# Using `*` instead of `+` for compatibility. The result is the same,
+# since the version to be bumped is extracted using `+`.
+new_version="$( echo $version | sed "s/[0-9]*$/$version_digit_to_bump/g" )"
+sed -i "" -e "s/$VERSION_NAME_PATTERN = .*$/$VERSION_NAME_PATTERN = $new_version-SNAPSHOT/g" $GRADLE_FILEPATH
