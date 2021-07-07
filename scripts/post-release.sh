98d25b3817fd115ec73a6457c7a9995848fda4fd
iker barriocanal <32816711+iker-barriocanal@users.noreply.github.com>
2021-07-07 12:58:15 +0200

    Rename version variable name

--- scripts/post-release.sh
+++ scripts/post-release.sh
@@ -20,11 +20,11 @@ sed -i "" 's/# Changelog/# Changelog\n\n## Unreleased/' CHANGELOG.md
 # Note that the version must end with a number: `1.2.3-alpha` is a semantic
 # version but not compatible with this post-release script, and `1.2.3-alpha.0`
 # should be used instead.
-VERSION_NAME_PATTERN="version"
-version="$( awk "/$VERSION_NAME_PATTERN/" $GRADLE_FILEPATH | egrep -o '[0-9].*$' )" # from the first digit until the end
-version_digit_to_bump="$( awk "/$VERSION_NAME_PATTERN/" $GRADLE_FILEPATH | egrep -o '[0-9]+$')"
+VERSION_PATTERN="version"
+version="$( awk "/$VERSION_PATTERN/" $GRADLE_FILEPATH | egrep -o '[0-9].*$' )" # from the first digit until the end
+version_digit_to_bump="$( awk "/$VERSION_PATTERN/" $GRADLE_FILEPATH | egrep -o '[0-9]+$')"
 ((version_digit_to_bump++))
 # Using `*` instead of `+` for compatibility. The result is the same,
 # since the version to be bumped is extracted using `+`.
 new_version="$( echo $version | sed "s/[0-9]*$/$version_digit_to_bump/g" )"
-sed -i "" -e "s/$VERSION_NAME_PATTERN = .*$/$VERSION_NAME_PATTERN = $new_version-SNAPSHOT/g" $GRADLE_FILEPATH
+sed -i "" -e "s/$VERSION_PATTERN = .*$/$VERSION_PATTERN = $new_version-SNAPSHOT/g" $GRADLE_FILEPATH
