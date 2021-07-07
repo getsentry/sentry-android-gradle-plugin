ef5eb045d5eccc10f582115536857c47fd7a86ae
iker barriocanal <32816711+iker-barriocanal@users.noreply.github.com>
2021-07-07 13:00:38 +0200

    Remove unnecessary `echo`s

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -7,9 +7,6 @@ cd $SCRIPT_DIR/..
 OLD_VERSION="${1}"
 NEW_VERSION="${2}"
 
-echo $OLD_VERSION
-echo $NEW_VERSION
-
 GRADLE_FILEPATH="plugin-build/gradle.properties"
 
 # Replace `version` with the given version
