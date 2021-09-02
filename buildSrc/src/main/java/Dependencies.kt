36bba1f43a6b2f8da9cf3f150aece1b5cff58d65
Roman Zavarnitsyn <rom4ek93@gmail.com>
2021-09-02 10:35:07 +0200

    feat: transform api agp (#174)

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,5 +1,5 @@
 object BuildPluginsVersion {
-    val AGP = System.getenv("VERSION_AGP") ?: "4.2.2"
+    val AGP = System.getenv("VERSION_AGP") ?: "7.0.1"
     const val DOKKA = "1.4.32"
     const val KOTLIN = "1.4.32"
     const val KTLINT = "10.0.0"
---
+++ plugin-build/src/main/kotlin/io/sentry/android/gradle/instrumentation/SpanAddingClassVisitorFactory.kt
@@ -0,0 +1,32 @@
+package io.sentry.android.gradle.instrumentation
+
+import com.android.build.api.instrumentation.*
+import org.gradle.api.tasks.Input
+import org.gradle.api.provider.Property
+import org.objectweb.asm.ClassVisitor
+
+@Suppress("UnstableApiUsage")
+abstract class SpanAddingClassVisitorFactory :
+    AsmClassVisitorFactory<SpanAddingClassVisitorFactory.SpanAddingParameters> {
+
+    override fun createClassVisitor(
+        classContext: ClassContext,
+        nextClassVisitor: ClassVisitor
+    ): ClassVisitor =
+        Instrumentable[classContext.currentClassData.className]
+            .getClassVisitor(instrumentationContext.apiVersion.get(), nextClassVisitor)
+
+    override fun isInstrumentable(classData: ClassData): Boolean =
+        classData.className in Instrumentable.names()
+
+    interface SpanAddingParameters : InstrumentationParameters {
+
+        /**
+         * AGP will re-instrument dependencies, when the [InstrumentationParameters] changed
+         * https://issuetracker.google.com/issues/190082518#comment4. This is just a dummy parameter
+         * that is used solely for that purpose.
+         */
+        @get:Input
+        val invalidate: Property<Long>
+    }
+}
