4da7d03e438dfa67a13dd008fcf55800b4a67545
Roman Zavarnitsyn <rom4ek93@gmail.com>
2021-09-27 16:52:54 +0200

    Implement automatic var counting

--- plugin-build/src/main/kotlin/io/sentry/android/gradle/instrumentation/util/VisitorUtils.kt
+++ plugin-build/src/main/kotlin/io/sentry/android/gradle/instrumentation/util/VisitorUtils.kt
@@ -1,6 +1,7 @@
 package io.sentry.android.gradle.instrumentation.util
 
 import org.objectweb.asm.Opcodes
+import org.objectweb.asm.Type
 
 enum class ReturnType(val loadInsn: Int, val storeInsn: Int, val returnInsn: Int) {
     INTEGER(Opcodes.ILOAD, Opcodes.ISTORE, Opcodes.IRETURN),
@@ -12,5 +13,17 @@ enum class ReturnType(val loadInsn: Int, val storeInsn: Int, val returnInsn: Int
 
     companion object {
         fun returnCodes(): List<Int> = values().map { it.returnInsn }
+
+        fun storeCodes(): List<Int> = values().map { it.storeInsn }
+
+        fun loadCodes(): List<Int> = values().map { it.loadInsn }
+
+        fun fromDescriptor(descriptor: String?): ReturnType {
+            descriptor ?: error("Unable to convert $descriptor to ReturnType")
+
+            val type = Type.getReturnType(descriptor).getOpcode(Opcodes.IRETURN)
+            return values().find { it.returnInsn == type }
+                ?: error("Unable to convert $descriptor to ReturnType")
+        }
     }
 }
