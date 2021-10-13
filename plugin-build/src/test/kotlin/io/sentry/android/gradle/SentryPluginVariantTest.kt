918a9ad0456cc7785177824e110dfce7a6467803
Manoel Aranda Neto <marandaneto@gmail.com>
2021-10-13 15:22:08 +0200

    remove ded code

--- plugin-build/src/test/kotlin/io/sentry/android/gradle/SentryPluginVariantTest.kt
+++ plugin-build/src/test/kotlin/io/sentry/android/gradle/SentryPluginVariantTest.kt
@@ -204,12 +204,6 @@ class SentryPluginVariantTest(
             // The supported Gradle version can be found here:
             // https://developer.android.com/studio/releases/gradle-plugin#updating-gradle
             // The pair is [AGP Version, Gradle Version]
-//            arrayOf("4.0.2", "6.1.1"),
-//            arrayOf("4.1.3", "6.5"),
-//            arrayOf("4.1.3", "6.8.3"),
-//            arrayOf("4.1.3", "7.0.2"),
-//            arrayOf("4.2.2", "6.8.3"),
-//            arrayOf("4.2.2", "7.0.2"),
             arrayOf("7.0.2", "7.0.2"),
             arrayOf("7.0.2", "7.1.1"),
             arrayOf("7.0.2", "7.2"),
