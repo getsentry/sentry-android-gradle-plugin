d6419d0d7bf67a4702505acf4c6b13a3b0e7e9f8
Manoel Aranda Neto <marandaneto@gmail.com>
2021-11-08 21:19:59 +0100

    Bump: AGP 7.1 beta and 7.2 alpha

--- plugin-build/src/test/kotlin/io/sentry/android/gradle/SentryPluginTest.kt
+++ plugin-build/src/test/kotlin/io/sentry/android/gradle/SentryPluginTest.kt
@@ -302,8 +302,8 @@ class SentryPluginTest(
             arrayOf("7.0.3", "7.0.2"),
             arrayOf("7.0.3", "7.1.1"),
             arrayOf("7.0.3", "7.2"),
-            arrayOf("7.1.0-beta01", "7.2"),
-            arrayOf("7.2.0-alpha01", "7.2")
+            arrayOf("7.1.0-beta02", "7.2"),
+            arrayOf("7.2.0-alpha03", "7.2")
         )
 
         private fun GradleRunner.appendArguments(vararg arguments: String) =
