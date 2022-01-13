b00ecaf87ea6563bb9afdee7b53b52e7586b8568
Roman Zavarnitsyn <rom4ek93@gmail.com>
2022-01-13 23:11:48 +0100

    Fix test

--- plugin-build/src/test/kotlin/io/sentry/android/gradle/instrumentation/fakes/TestSpanAddingParameters.kt
+++ plugin-build/src/test/kotlin/io/sentry/android/gradle/instrumentation/fakes/TestSpanAddingParameters.kt
@@ -1,34 +1,22 @@
 package io.sentry.android.gradle.instrumentation.fakes
 
+import com.nhaarman.mockitokotlin2.mock
 import io.sentry.android.gradle.InstrumentationFeature
 import io.sentry.android.gradle.instrumentation.ClassInstrumentable
 import io.sentry.android.gradle.instrumentation.SpanAddingClassVisitorFactory
 import java.io.File
 import org.gradle.api.file.RegularFileProperty
-import org.gradle.api.internal.file.DefaultFileCollectionFactory
 import org.gradle.api.internal.file.DefaultFileLookup
-import org.gradle.api.internal.file.DefaultFilePropertyFactory
-import org.gradle.api.internal.file.FileCollectionFactory
-import org.gradle.api.internal.file.collections.DefaultDirectoryFileTreeFactory
 import org.gradle.api.internal.provider.DefaultProperty
 import org.gradle.api.internal.provider.DefaultSetProperty
 import org.gradle.api.internal.provider.PropertyHost
-import org.gradle.api.internal.tasks.DefaultTaskDependencyFactory
 import org.gradle.api.provider.Property
 import org.gradle.api.provider.SetProperty
-import org.gradle.api.tasks.util.internal.PatternSets
-import org.gradle.internal.nativeintegration.services.FileSystems
-import org.gradle.internal.nativeintegration.services.NativeServices
 
 class TestSpanAddingParameters(
     private val debugOutput: Boolean = true,
-    private val inMemoryDir: File,
-    private val _sdkStateFile: File = File(inMemoryDir, "sdk-state.json")
+    private val inMemoryDir: File
 ) : SpanAddingClassVisitorFactory.SpanAddingParameters {
-
-    init {
-        NativeServices.initializeOnDaemon(inMemoryDir)
-    }
 
     override val invalidate: Property<Long>
         get() = DefaultProperty(PropertyHost.NO_OP, Long::class.java).convention(0L)
@@ -42,29 +30,10 @@ class TestSpanAddingParameters(
             .convention(setOf(InstrumentationFeature.FILE_IO, InstrumentationFeature.DATABASE))
 
     override val sdkStateFile: RegularFileProperty
-        get() = filePropertyFactory.newFileProperty().value(filePropertyFactory.file(_sdkStateFile))
+        get() = TODO()
 
     override val tmpDir: Property<File>
         get() = DefaultProperty<File>(PropertyHost.NO_OP, File::class.java).convention(inMemoryDir)
 
     override var _instrumentables: List<ClassInstrumentable>? = listOf()
-
-    private val fileLookup: DefaultFileLookup = DefaultFileLookup()
-
-    private val fileCollectionFactory: FileCollectionFactory =
-        DefaultFileCollectionFactory(
-            fileLookup.pathToFileResolver,
-            DefaultTaskDependencyFactory.withNoAssociatedProject(),
-            DefaultDirectoryFileTreeFactory(),
-            PatternSets.getNonCachingPatternSetFactory(),
-            PropertyHost.NO_OP,
-            FileSystems.getDefault()
-        )
-
-    private val filePropertyFactory: DefaultFilePropertyFactory =
-        DefaultFilePropertyFactory(
-            PropertyHost.NO_OP,
-            fileLookup.fileResolver,
-            fileCollectionFactory
-        )
 }
