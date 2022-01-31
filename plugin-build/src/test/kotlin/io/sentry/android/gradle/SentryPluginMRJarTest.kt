6157f69e3e4b423280fb23c139bf2b8fbd777dca
Roman Zavarnitsyn <rom4ek93@gmail.com>
2022-01-31 20:01:29 +0100

    Custom semver parsing

--- plugin-build/src/test/kotlin/io/sentry/android/gradle/SentryPluginMRJarTest.kt
+++ plugin-build/src/test/kotlin/io/sentry/android/gradle/SentryPluginMRJarTest.kt
@@ -1,4 +1,42 @@
 package io.sentry.android.gradle
 
-class SentryPluginMRJarTest {
+import kotlin.test.assertTrue
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+
+@RunWith(Parameterized::class)
+class SentryPluginMRJarTest(
+    androidGradlePluginVersion: String,
+    gradleVersion: String
+) : BaseSentryPluginTest(androidGradlePluginVersion, gradleVersion) {
+
+    @Test
+    fun `does not break when there is a MR-JAR dependency with unsupported java version`() {
+        appBuildFile.writeText(
+            // language=Groovy
+            """
+            plugins {
+              id "com.android.application"
+              id "io.sentry.android.gradle"
+            }
+
+            dependencies {
+              implementation("com.squareup.moshi:moshi-kotlin:1.13.0")
+            }
+
+            sentry.tracingInstrumentation.enabled = true
+            """.trimIndent()
+        )
+
+        val result = runner
+            .appendArguments("app:assembleDebug")
+            .build()
+
+        print(result.output)
+
+        assertTrue { "BUILD SUCCESSFUL" in result.output }
+    }
+
+    override val additionalRootProjectConfig: String = ""
 }
--- plugin-build/src/test/kotlin/io/sentry/android/gradle/util/AgpVersionsTest.kt
+++ plugin-build/src/test/kotlin/io/sentry/android/gradle/util/AgpVersionsTest.kt
@@ -8,7 +8,8 @@ class AgpVersionsTest {
 
     @Test
     fun `agp versions`() {
-        assertTrue { VersionNumber.parse("7.1.0") < AgpVersions.VERSION_7_2_0_alpha06 }
-        assertTrue { VersionNumber.parse("7.2.0") > AgpVersions.VERSION_7_2_0_alpha06 }
+        assertTrue { SemVer.parse("7.1.0") < AgpVersions.VERSION_7_2_0_alpha06 }
+        assertTrue { SemVer.parse("7.2.0-alpha07") > AgpVersions.VERSION_7_2_0_alpha06 }
+        assertTrue { SemVer.parse("7.2.0") > AgpVersions.VERSION_7_2_0_alpha06 }
     }
 }
