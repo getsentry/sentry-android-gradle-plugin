56484011213c83167fa81225caecb4e7da689b37
Roman Zavarnitsyn <rom4ek93@gmail.com>
2022-02-15 16:07:44 +0100

    Add auto-installation for sentry-android-timber

---
+++ plugin-build/src/main/kotlin/io/sentry/android/gradle/autoinstall/okhttp/OkHttpInstallStrategy.kt
@@ -0,0 +1,63 @@
+package io.sentry.android.gradle.autoinstall.okhttp
+
+import io.sentry.android.gradle.SentryPlugin
+import io.sentry.android.gradle.autoinstall.AutoInstallState
+import io.sentry.android.gradle.autoinstall.InstallStrategyRegistrar
+import io.sentry.android.gradle.autoinstall.SENTRY_GROUP
+import io.sentry.android.gradle.util.SemVer
+import io.sentry.android.gradle.util.info
+import javax.inject.Inject
+import org.gradle.api.artifacts.CacheableRule
+import org.gradle.api.artifacts.ComponentMetadataContext
+import org.gradle.api.artifacts.ComponentMetadataRule
+import org.gradle.api.artifacts.DependencySet
+import org.gradle.api.artifacts.dsl.ComponentMetadataHandler
+
+//@CacheableRule
+abstract class OkHttpInstallStrategy @Inject constructor(
+    private val autoInstallState: AutoInstallState
+) : ComponentMetadataRule {
+
+    override fun execute(context: ComponentMetadataContext) {
+        if (!autoInstallState.installOkHttp) {
+            SentryPlugin.logger.info {
+                "sentry-android-okhttp won't be installed because it was already installed directly"
+            }
+            return
+        }
+        val semVer = SemVer.parse(context.details.id.version)
+        if (semVer < MIN_SUPPORTED_VERSION) {
+            SentryPlugin.logger.info {
+                "$SENTRY_OKHTTP_ID won't be installed because the current okhttp is lower " +
+                    "than the minimum supported version ($MIN_SUPPORTED_VERSION)"
+            }
+            return
+        }
+
+        context.details.allVariants { metadata ->
+            metadata.withDependencies { dependencies ->
+                val sentryVersion = autoInstallState.sentryVersion
+                dependencies.add("$SENTRY_GROUP:$SENTRY_OKHTTP_ID:$sentryVersion")
+
+                SentryPlugin.logger.info {
+                    "$SENTRY_OKHTTP_ID is successfully installed with version: $sentryVersion"
+                }
+            }
+        }
+    }
+
+
+    companion object Registrar : InstallStrategyRegistrar {
+        private const val OKHTTP_GROUP = "com.squareup.okhttp3"
+        private const val OKHTTP_ID = "okhttp"
+        internal const val SENTRY_OKHTTP_ID = "sentry-android-okhttp"
+
+        private val MIN_SUPPORTED_VERSION = SemVer(3, 13, 0)
+
+        override fun register(component: ComponentMetadataHandler) {
+            component.withModule("$OKHTTP_GROUP:$OKHTTP_ID", OkHttpInstallStrategy::class.java) {
+                it.params(AutoInstallState)
+            }
+        }
+    }
+}
