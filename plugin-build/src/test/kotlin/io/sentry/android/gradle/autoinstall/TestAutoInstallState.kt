d6accf383714500a3ac411b4c683175bd9b4f742
Roman Zavarnitsyn <rom4ek93@gmail.com>
2022-02-18 20:32:15 +0100

    Add integration test

---
+++ plugin-build/src/test/kotlin/io/sentry/android/gradle/autoinstall/TestAutoInstallState.kt
@@ -0,0 +1,7 @@
+package io.sentry.android.gradle.autoinstall
+
+import org.gradle.api.services.BuildServiceParameters
+
+class TestAutoInstallState : AutoInstallState() {
+    override fun getParameters(): BuildServiceParameters.None = TODO()
+}
