d1a20113bc3a97133b448b21b685bdff757d767c
Denis Andrašec <denrase@gmail.com>
2022-09-05 14:51:58 +0000

    Enha: sentry-cli Integration Tests (#371)

---
+++ plugin-build/src/test/kotlin/io/sentry/android/gradle/SentryPluginIntegrationTest.kt
@@ -0,0 +1,90 @@
+package io.sentry.android.gradle
+
+import kotlin.test.assertEquals
+import org.gradle.testkit.runner.TaskOutcome
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+
+@Suppress("FunctionName")
+@RunWith(Parameterized::class)
+class SentryPluginIntegrationTest(
+    androidGradlePluginVersion: String,
+    gradleVersion: String
+) : BaseSentryPluginTest(androidGradlePluginVersion, gradleVersion) {
+
+    @Test
+    fun uploadSentryProguardMappingsIntegration() {
+        if (System.getenv("SENTRY_URL").isNullOrBlank()) {
+            return // Don't run test if local test server endpoint is not set
+        }
+        applyAutoUploadProguardMapping()
+
+        val build = runner
+            .appendArguments(":app:assembleRelease")
+            .build()
+
+        assertEquals(
+            build.task(":app:uploadSentryProguardMappingsRelease")?.outcome,
+            TaskOutcome.SUCCESS
+        )
+    }
+
+    @Test
+    fun uploadNativeSymbols() {
+        if (System.getenv("SENTRY_URL").isNullOrBlank()) {
+            return // Don't run test if local test server endpoint is not set
+        }
+        applyUploadNativeSymbols()
+
+        val build = runner
+            .appendArguments(":app:assembleRelease")
+            .build()
+
+        assertEquals(
+            build.task(":app:uploadSentryNativeSymbolsForRelease")?.outcome,
+            TaskOutcome.SUCCESS
+        )
+    }
+
+    private fun applyAutoUploadProguardMapping() {
+        appBuildFile.writeText(
+            // language=Groovy
+            """
+                plugins {
+                  id "com.android.application"
+                  id "io.sentry.android.gradle"
+                }
+
+                sentry {
+                  includeProguardMapping = true
+                  autoUploadProguardMapping = true
+                  uploadNativeSymbols = false
+                  tracingInstrumentation {
+                    enabled = false
+                  }
+                }
+            """.trimIndent()
+        )
+    }
+
+    private fun applyUploadNativeSymbols() {
+        appBuildFile.writeText(
+            // language=Groovy
+            """
+                plugins {
+                  id "com.android.application"
+                  id "io.sentry.android.gradle"
+                }
+
+                sentry {
+                  autoUploadProguardMapping = false
+                  uploadNativeSymbols = true
+                  tracingInstrumentation {
+                    enabled = false
+                  }
+                }
+            """.trimIndent()
+        )
+    }
+}
---
+++ test/integration-test-server-start.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+python3 test/integration-test-server.py
