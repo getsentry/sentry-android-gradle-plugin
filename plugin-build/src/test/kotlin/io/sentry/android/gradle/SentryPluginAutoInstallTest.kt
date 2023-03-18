aebc604f0968e3d0dec9fadb27bb1889114e813a
Roman Zavarnitsyn <rom4ek93@gmail.com>
2023-03-18 22:23:16 +0100

    Do not forward output on CI

--- plugin-build/src/test/kotlin/io/sentry/android/gradle/SentryPluginAutoInstallTest.kt
+++ plugin-build/src/test/kotlin/io/sentry/android/gradle/SentryPluginAutoInstallTest.kt
@@ -166,7 +166,5 @@ class SentryPluginAutoInstallTest(
         .appendArguments("app:dependencies")
         .appendArguments("--configuration")
         .appendArguments("debugRuntimeClasspath")
-        .appendArguments("--console=plain")
-        .forwardOutput()
         .build()
 }
