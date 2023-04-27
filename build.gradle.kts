c70d90ccaa7b0f45017999341c994802f3bcb6d9
Markus Hintersteiner <markus.hintersteiner@sentry.io>
2023-04-27 14:56:35 +0200

    Ensure kotlin compiler plugin get's published before running tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,6 +38,7 @@ tasks.register("clean", Delete::class.java) {
 tasks.register("preMerge") {
     description = "Runs all the tests/verification tasks on both top level and included build."
 
+    dependsOn(":sentry-kotlin-compiler-plugin:publishAllPublicationsToMavenTestRepoRepository")
     dependsOn(":examples:android-gradle:check")
     dependsOn(":examples:android-gradle-kts:check")
     dependsOn(":examples:android-ndk:check")
