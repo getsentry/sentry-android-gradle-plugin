1d85ac961968ecc90a3fcc87c6aeeeff5a56bbfa
Markus Hintersteiner <markus.hintersteiner@sentry.io>
2023-04-28 09:30:53 +0200

    Fix kapt/compile toolchain mismatch

--- examples/android-instrumentation-sample/build.gradle.kts
+++ examples/android-instrumentation-sample/build.gradle.kts
@@ -47,8 +47,12 @@ android {
         }
     }
 
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_1_8.toString()
+        jvmTarget = JavaVersion.VERSION_11.toString()
     }
     namespace = "io.sentry.samples.instrumentation"
 
@@ -59,6 +63,10 @@ android {
     composeOptions {
         kotlinCompilerExtensionVersion = "1.4.6"
     }
+}
+
+kotlin {
+    jvmToolchain(11)
 }
 
 // useful, when we want to modify room-generated classes, and then compile them into .class files
