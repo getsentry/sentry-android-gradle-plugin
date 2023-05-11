b0704a6c74e7eb7d97d75104be6c4ab5b6f88b95
Alexander Dinauer <alexander.dinauer@sentry.io>
2023-05-11 05:58:55 +0200

    Mark outputFile on PropertiesFileOutputTask internal

--- plugin-build/src/main/kotlin/io/sentry/android/gradle/tasks/PropertiesFileOutputTask.kt
+++ plugin-build/src/main/kotlin/io/sentry/android/gradle/tasks/PropertiesFileOutputTask.kt
@@ -4,8 +4,10 @@ import org.gradle.api.DefaultTask
 import org.gradle.api.file.DirectoryProperty
 import org.gradle.api.file.RegularFile
 import org.gradle.api.provider.Provider
+import org.gradle.api.tasks.Internal
 import org.gradle.api.tasks.OutputDirectory
 
 abstract class PropertiesFileOutputTask : DirectoryOutputTask() {
+    @get:Internal
     abstract val outputFile: Provider<RegularFile>;
 }
