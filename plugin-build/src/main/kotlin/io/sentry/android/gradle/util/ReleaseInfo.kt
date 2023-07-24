6944881ce9abb65e9c7ee68179b0f1d30f754cca
buenaflor <giancarlo.buenaflor@sentry.io>
2023-07-24 12:05:04 +0200

    Make ReleaseInfo serializable

--- plugin-build/src/main/kotlin/io/sentry/android/gradle/util/ReleaseInfo.kt
+++ plugin-build/src/main/kotlin/io/sentry/android/gradle/util/ReleaseInfo.kt
@@ -1,7 +1,9 @@
 package io.sentry.android.gradle.util
 
+import java.io.Serializable
+
 data class ReleaseInfo(
     val applicationId: String,
     val versionName: String,
     val versionCode: Int? = null
-)
+) : Serializable
