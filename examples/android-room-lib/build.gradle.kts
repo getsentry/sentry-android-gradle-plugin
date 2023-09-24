c5f85852f2a79df8e63897061ce0e1ff4ab8c6e9
Roman Zavarnitsyn <rom4ek93@gmail.com>
2023-09-24 18:30:24 +0200

    wip

--- examples/android-room-lib/build.gradle.kts
+++ examples/android-room-lib/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = JavaVersion.VERSION_1_8.toString()
     }
     namespace = "io.sentry.android.instrumentation.lib"
 }
