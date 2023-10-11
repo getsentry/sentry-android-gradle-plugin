a309a6ded481e2d3932c04c69da234122fd0e44c
Roman Zavarnitsyn <rom4ek93@gmail.com>
2023-10-11 22:00:34 +0200

    Dont run config-cache tests on gradle below 8.0 due to a bug

--- plugin-build/src/test/kotlin/io/sentry/android/gradle/integration/SentryPluginSourceContextTest.kt
+++ plugin-build/src/test/kotlin/io/sentry/android/gradle/integration/SentryPluginSourceContextTest.kt
@@ -10,8 +10,8 @@ import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 import org.gradle.testkit.runner.TaskOutcome.SKIPPED
 import org.gradle.util.GradleVersion
-import org.hamcrest.CoreMatchers
-import org.junit.Assume
+import org.hamcrest.CoreMatchers.`is`
+import org.junit.Assume.assumeThat
 import org.junit.Test
 
 class SentryPluginSourceContextTest :
@@ -118,11 +118,12 @@ class SentryPluginSourceContextTest :
 
     @Test
     fun `respects configuration cache`() {
-        Assume.assumeThat(
-            "SentryExternalDependenciesReportTask only supports " +
-                "configuration cache from Gradle 7.5 onwards",
-            GradleVersions.CURRENT >= GradleVersions.VERSION_7_5,
-            CoreMatchers.`is`(true)
+        assumeThat(
+            "Sentry Source Context only supports " +
+                "configuration cache from Gradle 8.0 onwards due to the bug in gradle " +
+                "https://github.com/gradle/gradle/issues/19252",
+            GradleVersions.CURRENT >= GradleVersions.VERSION_8_0,
+            `is`(true)
         )
 
         appBuildFile.writeText(
