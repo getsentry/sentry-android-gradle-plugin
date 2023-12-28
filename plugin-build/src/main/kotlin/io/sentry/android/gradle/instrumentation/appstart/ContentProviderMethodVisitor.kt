b3231c7c50f97d06dcb3e489547725fe3cf9237b
Markus Hintersteiner <markus.hintersteiner@sentry.io>
2023-12-28 10:09:37 +0100

    First success

--- plugin-build/src/main/kotlin/io/sentry/android/gradle/instrumentation/appstart/ContentProviderMethodVisitor.kt
+++ plugin-build/src/main/kotlin/io/sentry/android/gradle/instrumentation/appstart/ContentProviderMethodVisitor.kt
@@ -1,9 +1,25 @@
 package io.sentry.android.gradle.instrumentation.appstart
 
 import io.sentry.android.gradle.instrumentation.MethodContext
+import io.sentry.android.gradle.instrumentation.util.Types
 import org.objectweb.asm.MethodVisitor
+import org.objectweb.asm.Type
 import org.objectweb.asm.commons.AdviceAdapter
 
+/**
+ * Decorates the onCreate method of a ContentProvider and adds Sentry performance monitoring by
+ * calling AppStartMetrics.onContentProviderCreate and AppStartMetrics.onContentProviderPostCreate
+ *
+ * Due to bytecode optimization of some popular libraries (e.g. androidx.startup, MlKit)
+ * we can't trust that the instrumented bytecode conforms to the Java bytecode specification.
+ *
+ * E.g. the following is no longer true
+ * Quoting docs.oracle.com/javase/specs/jvms/se7/html/jvms-2.html#jvms-2.6.1:
+ * On instance method invocation, local variable 0 is always used to pass a reference
+ * to the object on which the instance method is being invoked
+ * (this in the Java programming language).
+ *
+ */
 class ContentProviderMethodVisitor(
     apiVersion: Int,
     originalVisitor: MethodVisitor,
@@ -16,10 +32,24 @@ class ContentProviderMethodVisitor(
     instrumentableContext.descriptor
 ) {
 
-    override fun onMethodEnter() {
-        super.onMethodEnter()
+    private var counterIdx = 0
+    private var safeThisIdx = 0
 
-        loadThis()
+    override fun onMethodEnter() {
+        newLocal(Types.OBJECT)
+        counterIdx = newLocal(Types.INT)
+        safeThisIdx = newLocal(Types.OBJECT)
+
+        // finally load this and store it in the local variable
+        visitVarInsn(ALOAD, 0)
+        visitVarInsn(ASTORE, safeThisIdx)
+
+        visitInsn(ICONST_5)
+        visitVarInsn(ISTORE, counterIdx)
+
+        visitVarInsn(ALOAD, safeThisIdx)
+        box(Type.getType("Landroid/content/ContentProvider;"))
+
         visitMethodInsn(
             INVOKESTATIC,
             "io/sentry/android/core/performance/AppStartMetrics",
@@ -30,9 +60,8 @@ class ContentProviderMethodVisitor(
     }
 
     override fun onMethodExit(opcode: Int) {
-        super.onMethodExit(opcode)
-
-        loadThis()
+        visitVarInsn(ALOAD, safeThisIdx)
+        box(Type.getType("Landroid/content/ContentProvider;"))
         visitMethodInsn(
             INVOKESTATIC,
             "io/sentry/android/core/performance/AppStartMetrics",
@@ -40,5 +69,7 @@ class ContentProviderMethodVisitor(
             "(Landroid/content/ContentProvider;)V",
             false
         )
+        visitInsn(ICONST_3)
+        visitVarInsn(ISTORE, counterIdx)
     }
 }
