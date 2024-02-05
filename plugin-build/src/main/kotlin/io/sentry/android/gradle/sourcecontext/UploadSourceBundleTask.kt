fa1714d4cc0698cc4f81597c257ee295d19b5d29
Lukas Bloder <lukas.bloder@gmail.com>
2024-02-05 10:36:43 +0100

    add clarifying comments for upToDateWhen

--- plugin-build/src/main/kotlin/io/sentry/android/gradle/sourcecontext/UploadSourceBundleTask.kt
+++ plugin-build/src/main/kotlin/io/sentry/android/gradle/sourcecontext/UploadSourceBundleTask.kt
@@ -35,6 +35,9 @@ abstract class UploadSourceBundleTask : Exec() {
         }
 
         // Allows gradle to consider this task up-to-date if the inputs haven't changed
+        // As this task does not have any outputs, it will always be considered to be out-of-date otherwise
+        // More info here https://docs.gradle.org/current/userguide/more_about_tasks.html#sec:task_outcomes
+        // and https://docs.gradle.org/current/userguide/incremental_build.html#sec:custom_up_to_date_logic
         outputs.upToDateWhen { true }
     }
 
