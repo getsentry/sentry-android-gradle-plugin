41ec302d46aa5df19d7323bcb743ee050043f9bb
Markus Hintersteiner <markus.hintersteiner@sentry.io>
2024-04-04 12:43:11 +0200

    Attribute code inspiration

--- plugin-build/src/main/kotlin/io/sentry/android/gradle/tasks/InjectSentryMetadataIntoAssetsTask.kt
+++ plugin-build/src/main/kotlin/io/sentry/android/gradle/tasks/InjectSentryMetadataIntoAssetsTask.kt
@@ -1,3 +1,20 @@
+// Adapted from https://github.com/android/gradle-recipes/blob/efeedbc78465547280b5c13b3e04a65b70fa1e26/transformDirectory/build-logic/plugins/src/main/kotlin/TransformAssetsTask.kt
+/*
+ * Copyright (C) 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.sentry.android.gradle.tasks
 
 import io.sentry.android.gradle.extensions.SentryPluginExtension
