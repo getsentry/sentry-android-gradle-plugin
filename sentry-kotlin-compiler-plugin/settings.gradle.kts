028b7f02044e5b0bcda7c8c66394a6478d83bab9
Nelson Osacky <nelson@emergetools.com>
2025-05-15 08:38:10 +0200

    Centralize dependency management
    This removes some uses of the allprojects block in favor of centralized
    dependency management.

--- sentry-kotlin-compiler-plugin/settings.gradle.kts
+++ sentry-kotlin-compiler-plugin/settings.gradle.kts
@@ -1,1 +1,10 @@
 rootProject.name = ("sentry-kotlin-compiler-plugin")
+
+dependencyResolutionManagement {
+  repositories {
+    google()
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+  }
+  repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+}
