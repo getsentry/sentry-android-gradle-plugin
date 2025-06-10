85cc9e8e1aab292a542284b42051c8aa5f23c9fa
Nelson Osacky <nelson@emergetools.com>
2025-06-10 20:39:36 +0200

    Remove Gradle 7.5 and below compat

--- plugin-build/src/main/kotlin/io/sentry/android/gradle/tasks/dependencies/SentryExternalDependenciesReportTaskFactory.kt
+++ plugin-build/src/main/kotlin/io/sentry/android/gradle/tasks/dependencies/SentryExternalDependenciesReportTaskFactory.kt
@@ -3,7 +3,6 @@ package io.sentry.android.gradle.tasks.dependencies
 import io.sentry.android.gradle.extensions.SentryPluginExtension
 import io.sentry.android.gradle.tasks.DirectoryOutputTask
 import io.sentry.android.gradle.telemetry.SentryTelemetryService
-import io.sentry.android.gradle.util.GradleVersions
 import org.gradle.api.Project
 import org.gradle.api.file.Directory
 import org.gradle.api.provider.Provider
@@ -25,28 +24,15 @@ object SentryExternalDependenciesReportTaskFactory {
   ): TaskProvider<out DirectoryOutputTask> {
     // gradle 7.5 supports passing configuration resolution as task input and respects config
     // cache, so we have a different implementation from that version onwards
-    return if (GradleVersions.CURRENT >= GradleVersions.VERSION_7_5) {
-      SentryExternalDependenciesReportTaskV2.register(
-        project,
-        extension,
-        sentryTelemetryProvider,
-        configurationName,
-        attributeValueJar,
-        output,
-        includeReport,
-        taskSuffix,
-      )
-    } else {
-      SentryExternalDependenciesReportTask.register(
-        project,
-        extension,
-        sentryTelemetryProvider,
-        configurationName,
-        attributeValueJar,
-        output,
-        includeReport,
-        taskSuffix,
-      )
-    }
+    return SentryExternalDependenciesReportTaskV2.register(
+      project,
+      extension,
+      sentryTelemetryProvider,
+      configurationName,
+      attributeValueJar,
+      output,
+      includeReport,
+      taskSuffix,
+    )
   }
 }
