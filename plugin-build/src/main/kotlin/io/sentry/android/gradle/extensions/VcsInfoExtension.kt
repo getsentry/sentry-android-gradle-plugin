f385eb46f8d917a846c25f6d12bc7a17a9b148ee
Nelson Osacky <nelson@emergetools.com>
2025-08-29 16:19:47 +0200

    Add VCS info extension for build uploads
    - Create VcsInfoExtension with vcs related sentry-cli build upload
      params
    - Add vcsInfo property and configuration function to SentryPluginExtension
    - Update SentryUploadAppArtifactTask to pass VCS parameters to sentry-cli
    - Support head/base SHA, repo names, refs, VCS provider, and PR number

---
+++ plugin-build/src/main/kotlin/io/sentry/android/gradle/extensions/VcsInfoExtension.kt
@@ -0,0 +1,55 @@
+package io.sentry.android.gradle.extensions
+
+import javax.inject.Inject
+import org.gradle.api.model.ObjectFactory
+import org.gradle.api.provider.Property
+
+open class VcsInfoExtension @Inject constructor(objects: ObjectFactory) {
+
+  /**
+   * The VCS commit sha to use for the upload. If not provided, the current commit sha will be used.
+   */
+  val headSha: Property<String> = objects.property(String::class.java).convention(null as String?)
+
+  /**
+   * The VCS commit's base sha to use for the upload. If not provided, the merge-base of the current
+   * and remote branch will be used.
+   */
+  val baseSha: Property<String> = objects.property(String::class.java).convention(null as String?)
+
+  /** The VCS provider to use for the upload. If not provided, the current provider will be used. */
+  val vcsProvider: Property<String> =
+    objects.property(String::class.java).convention(null as String?)
+
+  /**
+   * The name of the git repository to use for the upload (e.g. organization/repository). If not
+   * provided, the current repository will be used.
+   */
+  val headRepoName: Property<String> =
+    objects.property(String::class.java).convention(null as String?)
+
+  /**
+   * The name of the git repository to use for the upload (e.g. organization/repository). If not
+   * provided, the current repository will be used.
+   */
+  val baseRepoName: Property<String> =
+    objects.property(String::class.java).convention(null as String?)
+
+  /**
+   * The reference (branch) to use for the upload. If not provided, the current reference will be
+   * used.
+   */
+  val headRef: Property<String> = objects.property(String::class.java).convention(null as String?)
+
+  /**
+   * The base reference (branch) to use for the upload. If not provided, the merge-base with the
+   * remote tracking branch will be used.
+   */
+  val baseRef: Property<String> = objects.property(String::class.java).convention(null as String?)
+
+  /**
+   * The pull request number to use for the upload. If not provided, the current pull request number
+   * will be used.
+   */
+  val prNumber: Property<Int> = objects.property(Int::class.java).convention(null as Int?)
+}
