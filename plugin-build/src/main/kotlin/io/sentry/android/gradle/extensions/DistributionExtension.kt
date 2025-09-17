569a2a208243ff61d2ae41597a57411e4bf71000
Nelson Osacky <nelson@emergetools.com>
2025-09-17 10:02:39 +0200

    Apply spotless formatting

--- plugin-build/src/main/kotlin/io/sentry/android/gradle/extensions/DistributionExtension.kt
+++ plugin-build/src/main/kotlin/io/sentry/android/gradle/extensions/DistributionExtension.kt
@@ -6,12 +6,9 @@ import org.gradle.api.provider.SetProperty
 import org.jetbrains.annotations.ApiStatus.Experimental
 
 @Experimental
-open class DistributionExtension
-@Inject
-constructor(objects: ObjectFactory) {
+open class DistributionExtension @Inject constructor(objects: ObjectFactory) {
 
-  /**
-   * Set of Android build variants that should have build distribution enabled.
-   */
-  val enabledFor: SetProperty<String> = objects.setProperty(String::class.java).convention(emptySet())
+  /** Set of Android build variants that should have build distribution enabled. */
+  val enabledFor: SetProperty<String> =
+    objects.setProperty(String::class.java).convention(emptySet())
 }
