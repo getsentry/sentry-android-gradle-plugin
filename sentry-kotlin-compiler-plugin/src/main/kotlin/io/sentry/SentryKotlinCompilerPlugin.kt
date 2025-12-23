242dcbb64cb473c7fa59ef6135ec55dfe9635140
Roman Zavarnitsyn <rom4ek93@gmail.com>
2025-12-23 10:44:43 +0100

    fix(compiler): Fix compatibility with Kotlin 2.3.0 (#1046)
    * fix(compiler): Fix compatibility with Kotlin 2.3.0
    
    * Changelog

--- sentry-kotlin-compiler-plugin/src/main/kotlin/io/sentry/SentryKotlinCompilerPlugin.kt
+++ sentry-kotlin-compiler-plugin/src/main/kotlin/io/sentry/SentryKotlinCompilerPlugin.kt
@@ -11,6 +11,7 @@ import org.jetbrains.kotlin.compiler.plugin.CompilerPluginRegistrar
 import org.jetbrains.kotlin.compiler.plugin.ExperimentalCompilerApi
 import org.jetbrains.kotlin.config.CompilerConfiguration
 import org.jetbrains.kotlin.config.KotlinCompilerVersion
+import org.jetbrains.kotlin.config.messageCollector
 
 @OptIn(ExperimentalCompilerApi::class)
 @AutoService(CompilerPluginRegistrar::class)
@@ -19,10 +20,9 @@ class SentryKotlinCompilerPlugin : CompilerPluginRegistrar() {
   override val supportsK2: Boolean
     get() = true
 
-  override fun ExtensionStorage.registerExtensions(configuration: CompilerConfiguration) {
-    val messageCollector =
-      configuration.get(CLIConfigurationKeys.MESSAGE_COLLECTOR_KEY, MessageCollector.NONE)
+  val pluginId: String = PLUGIN_ID
 
+  override fun ExtensionStorage.registerExtensions(configuration: CompilerConfiguration) {
     val versionString = KotlinCompilerVersion.getVersion()
     val version =
       if (versionString != null) {
@@ -33,13 +33,17 @@ class SentryKotlinCompilerPlugin : CompilerPluginRegistrar() {
 
     val extension: IrGenerationExtension =
       if (version >= SimpleSemVer(2, 2, 0)) {
+        val messageCollector = configuration.messageCollector
         JetpackComposeTracingIrExtension22(messageCollector)
       } else if (version >= SimpleSemVer(2, 1, 20)) {
+        val messageCollector = configuration.messageCollector
         // 2.1.20 removed some optional parameters, causing API incompatibility
         // e.g. java.lang.NoSuchMethodError
         // see https://github.com/JetBrains/kotlin/commit/dd508452c414a0ee8082aa6f76d664271cb38f2f
         JetpackComposeTracingIrExtension21(messageCollector)
       } else {
+        val messageCollector =
+          configuration.get(CLIConfigurationKeys.MESSAGE_COLLECTOR_KEY, MessageCollector.NONE)
         JetpackComposeTracingIrExtension19(messageCollector)
       }
 
