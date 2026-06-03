941fbe63367028caea870b0b325b3e554e740b5f
Adam Brown <adam.brown@sentry.io>
2026-06-03 14:23:37 +0200

    chore: remove make-it artifacts

--- .make-it/prototype/stubs/androidx/sqlite/SQLiteDriver.java
+++
@@ -1,6 +0,0 @@
-package androidx.sqlite;
-
-/** Minimal stub of androidx.sqlite.SQLiteDriver (the interface) for the prototype. */
-public interface SQLiteDriver {
-  Object open(String fileName);
-}
