2146342efba469c0302828494c3a4ef7f2a7f24e
Nicklas Ansman <nicklas@openai.com>
2026-08-19 15:33:19 -0400

    feat(instrumentation): allow disabling instrumentation for specific variants

--- plugin-build/src/main/kotlin/io/sentry/android/gradle/extensions/RuntimeOptimizationsExtension.kt
+++ plugin-build/src/main/kotlin/io/sentry/android/gradle/extensions/RuntimeOptimizationsExtension.kt
@@ -3,10 +3,23 @@ package io.sentry.android.gradle.extensions
 import javax.inject.Inject
 import org.gradle.api.model.ObjectFactory
 import org.gradle.api.provider.Property
+import org.gradle.api.provider.SetProperty
 
 open class RuntimeOptimizationsExtension @Inject constructor(objects: ObjectFactory) {
   /**
    * Enables runtime optimizations of the Sentry SDK at the cost of build time. Defaults to true.
    */
   val enabled: Property<Boolean> = objects.property(Boolean::class.java).convention(true)
+
+  /** Android build variants for which runtime optimizations should be disabled. */
+  val ignoredVariants: SetProperty<String> =
+    objects.setProperty(String::class.java).convention(emptySet())
+
+  /** Android build types for which runtime optimizations should be disabled. */
+  val ignoredBuildTypes: SetProperty<String> =
+    objects.setProperty(String::class.java).convention(emptySet())
+
+  /** Android build flavors for which runtime optimizations should be disabled. */
+  val ignoredFlavors: SetProperty<String> =
+    objects.setProperty(String::class.java).convention(emptySet())
 }
